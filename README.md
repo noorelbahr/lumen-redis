b0eea43ea89994cf7d8e89a670020a563a2d6a1b
Nurul Bahri <nurulbahri@hotmail.com>
2020-02-25 22:16:04 +0700

    Modify roles seeder and add Getting Started README.md file

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-## Building API Service for News Management with Lumen ##
-###### The stunningly fast micro-framework by Laravel. More info at https://lumen.laravel.com ######
+# Building API Service for News Management with Lumen #
+The stunningly fast micro-framework by Laravel. More info at https://lumen.laravel.com
+
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Set up Lumen project
+
+Copy `.env.example` file to `.env`
+```
+cp .env.example .env
+```
+
+Install dependencies
+```
+composer install
+```
+
+Then, we have to migrate our migration files to create our tables and seed default data for `users` and `roles`
+(in `database/migrations` and `database/seeds`)
+```
+php artisan migrate && php artisan db:seed
+```
+
+
+##### Install Laravel Passport
+In order to make laravel passport work for Lumen, we are going to use `dusterio/lumen-passport` package, a simple service provider for Laravel Passport. 
+For more detail visit https://github.com/dusterio/lumen-passport
+
+Then, run command below to install passport
+```
+php artisan passport:install
+```
+It will generates 2 clients for us
+```
+Personal access client created successfully.
+Client ID: 1
+Client secret: bo72cMzHsoXcIU5sTIYIZzD0qlfSa6VYJf8J5KYk
+Password grant client created successfully.
+Client ID: 2
+Client secret: 6UzPDESm8pQ4o474cKh6MPFLLBK0LgfiB3rLjjSu
+```
+
+##### Create Symlink for Storage Public Folder
+Lumen's artisan doesn't support `storage`, that's why we can't use `php artisan storage:link` in Lumen.
+
+So, we have to create it manually with `ln -s source_file symbolic_link`, example:
+```
+ln -s ~/full/path/to/lumen-passport/storage/app/public ~/full/path/to/lument-passport/public/storage
+```
+Replace `~/full/path/to/` with your full path of your project directory.
+
+##### Run Our Project
+Run command below to serve our project locally, we are going to use `port 8888`
+```
+php -S localhost:8888 -t public
+```
+Now we can access our project with url http://localhost:8888
+
+
+### Install Redis
+In this project, i used `redis queue` to for queuing the comment creation process.
+
+So, we have to install redis server with following command 
+```
+brew install redis
+```
+More instructions [click here](https://medium.com/@petehouston/install-and-config-redis-on-mac-os-x-via-homebrew-eb8df9a4f298)
+
+Test redis server
+```
+redis-cli ping
+```
+If it replies `PONG`, then it works!
+
+## Testing Our API
+To test our API, visit Postman Public Link below
+
+link
+
+You can test every single endpoint in the postman collection
+
+#### Test Redis Queue
+
--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -13,56 +13,55 @@ class RolesTableSeeder extends Seeder
     public function run()
     {
         Role::create([
-            [
-                'slug' => 'admin',
-                'name' => 'Admin',
-                'permissions' => json_encode([
-                    'users.list' => true,
-                    'users.detail' => true,
-                    'users.create' => true,
-                    'users.update' => true,
-                    'users.delete' => true,
-                    'roles.list' => true,
-                    'roles.detail' => true,
-                    'roles.create' => true,
-                    'roles.update' => true,
-                    'roles.delete' => true,
-                    'roles.permissions' => true,
-                    'news.list' => true,
-                    'news.detail' => true,
-                    'news.create' => true,
-                    'news.update' => true,
-                    'news.delete' => true,
-                    'news.comment' => true,
-                    'news.like' => true,
-                    'news.unlike' => true
-                ])
-            ],
-            [
-                'slug' => 'user',
-                'name' => 'User',
-                'permissions' => json_encode([
-                    'users.list' => false,
-                    'users.detail' => false,
-                    'users.create' => false,
-                    'users.update' => false,
-                    'users.delete' => false,
-                    'roles.list' => false,
-                    'roles.detail' => false,
-                    'roles.create' => false,
-                    'roles.update' => false,
-                    'roles.delete' => false,
-                    'roles.permissions' => false,
-                    'news.list' => true,
-                    'news.detail' => true,
-                    'news.create' => false,
-                    'news.update' => false,
-                    'news.delete' => false,
-                    'news.comment' => true,
-                    'news.like' => true,
-                    'news.unlike' => true
-                ])
-            ]
+            'slug' => 'admin',
+            'name' => 'Admin',
+            'permissions' => json_encode([
+                'users.list' => true,
+                'users.detail' => true,
+                'users.create' => true,
+                'users.update' => true,
+                'users.delete' => true,
+                'roles.list' => true,
+                'roles.detail' => true,
+                'roles.create' => true,
+                'roles.update' => true,
+                'roles.delete' => true,
+                'roles.permissions' => true,
+                'news.list' => true,
+                'news.detail' => true,
+                'news.create' => true,
+                'news.update' => true,
+                'news.delete' => true,
+                'news.comment' => true,
+                'news.like' => true,
+                'news.unlike' => true
+            ])
+        ]);
+
+        Role::create([
+            'slug' => 'user',
+            'name' => 'User',
+            'permissions' => json_encode([
+                'users.list' => false,
+                'users.detail' => false,
+                'users.create' => false,
+                'users.update' => false,
+                'users.delete' => false,
+                'roles.list' => false,
+                'roles.detail' => false,
+                'roles.create' => false,
+                'roles.update' => false,
+                'roles.delete' => false,
+                'roles.permissions' => false,
+                'news.list' => true,
+                'news.detail' => true,
+                'news.create' => false,
+                'news.update' => false,
+                'news.delete' => false,
+                'news.comment' => true,
+                'news.like' => true,
+                'news.unlike' => true
+            ])
         ]);
     }
 }
